8ad3166d1f803919f0bd25fda406076b5f1387bb
Wuelfhis Asuaje <wasuaje@shorecg.com>
2023-05-03 16:14:39 -0300

    accepting parameter

--- archive.py
+++ archive.py
@@ -3,8 +3,11 @@ from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 from azure.core.exceptions import ResourceExistsError
 import glob
+import sys
 
-containers = ["todocasero", "luma02", "luma01", "bosquejo"]
+source_folder = sys.argv[1] if len(sys.argv) > 1 else '.'
+# containers = ["todocasero", "luma02", "luma01", "bosquejo"]
+containers = ["todocasero"]
 
 # environment variable into account.
 connect_str = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
@@ -22,14 +25,15 @@ for container in containers:
         print("Container already exists...moving on!")
     finally:
         # Create a blob client using the local file name as the name for the blob
-        filenames_list = glob.glob(f'{container}-backup*.sql')
+        filenames_list = glob.glob(f'{source_folder}/{container}-backup*.sql')
         for file_name in filenames_list:
+            blob_name = file_name.split('/')[-1]
             blob_client = blob_service_client.get_blob_client(
-                container=container_name, blob=file_name)
+                container=container_name, blob=blob_name)
             try:
                 # Upload the created file
                 if not blob_client.exists():
-                    print("\nUploading to Azure Storage as blob:\n\t" + file_name)
+                    print("\nUploading to Azure Storage as blob:\n\t" + blob_name)
                     with open(file_name, "rb") as blob_bytes:
                         blob_client.upload_blob(blob_bytes)
             except Exception as ex:
